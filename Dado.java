88a6c4a741c6da9c3ee00a3f78f5aab524915762
Jonialen <77712004+Jonialen@users.noreply.github.com>
2023-08-04 20:46:09 -0600

    2

--- Dado.java
+++ Dado.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 // Clase Dado que representa un dado
 public class Dado {
-    // Método para simular el lanzamiento de un dado y obtener un número aleatorio entre 0 y 6
+    // Método para simular el lanzamiento de un dado y obtener un número aleatorio entre 1 y 6
     public int dado() {
         Random random = new Random(); // Crear un objeto de la clase Random para generar números aleatorios
 
